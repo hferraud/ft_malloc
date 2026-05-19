349ddf84656d23ce76984f3af0f88460d1b94f40
hferraud <ferraudhugo@gmail.com>
2026-05-19 14:01:36 +0200

    test: add ceedling test suite

---
+++ ceedling/test/test_chunk_create.c
@@ -0,0 +1,54 @@
+#include "unity.h"
+
+#include "../src/malloc.c"
+
+static void chunk_create_test(size_t size);
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_chunk_create_single(void) {
+    chunk_create_test(SMALL_CHUNK_SIZE + 1);
+    chunk_create_test(SMALL_CHUNK_SIZE * 2);
+    chunk_create_test(SMALL_CHUNK_SIZE * 2 - 1);
+    chunk_create_test(SMALL_CHUNK_SIZE * 128);
+}
+
+static void chunk_create_test(size_t size) {
+    chunk_t chunk;
+    chunk_t head = NULL;
+
+    chunk = chunk_create(&head, size);
+    TEST_ASSERT_NOT_NULL(chunk);
+    TEST_ASSERT_EQUAL(chunk->size, size);
+    TEST_ASSERT_NULL(chunk->next);
+    TEST_ASSERT_NULL(chunk->prev);
+    TEST_ASSERT_EQUAL(chunk->free, 0);
+    TEST_ASSERT_EQUAL(chunk->data, (void*)chunk + CHUNK_METADATA_SIZE);
+    TEST_ASSERT_EQUAL(MAGIC_DESERIALIZE(chunk->magic), chunk->data);
+}
+
+void test_chunk_create_multiple(void) {
+    chunk_t chunk;
+    chunk_t prev;
+    chunk_t head = NULL;
+
+    chunk = chunk_create(&head, SMALL_CHUNK_SIZE * 1);
+    TEST_ASSERT_NOT_NULL(chunk);
+    TEST_ASSERT_NULL(chunk->next);
+    TEST_ASSERT_NULL(chunk->prev);
+    prev = chunk;
+    chunk = chunk_create(&head, SMALL_CHUNK_SIZE * 2);
+    TEST_ASSERT_NOT_NULL(chunk);
+    TEST_ASSERT_NULL(chunk->next);
+    TEST_ASSERT_EQUAL(chunk->prev, prev);
+    TEST_ASSERT_EQUAL(prev->next, chunk);
+    TEST_ASSERT_NULL(prev->prev);
+    prev = chunk;
+    chunk = chunk_create(&head, SMALL_CHUNK_SIZE * 2);
+    TEST_ASSERT_NOT_NULL(chunk);
+    TEST_ASSERT_NULL(chunk->next);
+    TEST_ASSERT_EQUAL(chunk->prev, prev);
+    TEST_ASSERT_EQUAL(prev->next, chunk);
+    TEST_ASSERT_EQUAL(prev->prev, head);
+}
---
+++ ceedling/test/test_zone_create.c
@@ -0,0 +1,46 @@
+#include "unity.h"
+
+#include "../src/malloc.c"
+
+static void zone_create_test(size_t requested_size, size_t chunk_size);
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_zone_create_tiny(void) {
+    zone_create_test(0, TINY_CHUNK_SIZE);
+    zone_create_test(TINY_CHUNK_SIZE / 2, TINY_CHUNK_SIZE);
+    zone_create_test(TINY_CHUNK_SIZE, TINY_CHUNK_SIZE);
+}
+
+void test_zone_create_small(void) {
+    zone_create_test(TINY_CHUNK_SIZE + 1, SMALL_CHUNK_SIZE);
+    zone_create_test(SMALL_CHUNK_SIZE / 2, SMALL_CHUNK_SIZE);
+    zone_create_test(SMALL_CHUNK_SIZE, SMALL_CHUNK_SIZE);
+}
+
+void test_zone_create_large(void) {
+    TEST_ASSERT_NULL(zone_create(NULL, SMALL_CHUNK_SIZE + 1));
+}
+
+static void zone_create_test(size_t requested_size, size_t chunk_size) {
+    zone_t zone;
+    zone_t last;
+    const size_t page_size = sysconf(_SC_PAGESIZE);;
+
+    zone = zone_create(NULL, requested_size);
+    TEST_ASSERT_NOT_NULL(zone);
+    TEST_ASSERT_NULL(zone->next);
+    TEST_ASSERT_GREATER_OR_EQUAL(chunk_size * CHUNK_PER_ZONE, zone->size);
+    //Zone size should be page-aligned
+    TEST_ASSERT_EQUAL(zone->size % page_size, 0);
+    TEST_ASSERT_GREATER_OR_EQUAL(requested_size, zone->size);
+    last = zone;
+    zone = zone_create(last, requested_size);
+    TEST_ASSERT_NOT_NULL(zone);
+    TEST_ASSERT_NULL(zone->next);
+    TEST_ASSERT_GREATER_OR_EQUAL(chunk_size * CHUNK_PER_ZONE, zone->size);
+    //Zone size should be page-aligned
+    TEST_ASSERT_EQUAL(zone->size % page_size, 0);
+    TEST_ASSERT_EQUAL(last->next, zone);
+}
